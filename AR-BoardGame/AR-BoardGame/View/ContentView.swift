23e2fa7b76267036d1f5e7224650a25e94bf8c3e
daminoworld <ekadls757@gmail.com>
2024-09-05 09:26:48 +0900

    Feat: Modify ContentView

--- AR-BoardGame/AR-BoardGame/View/ContentView.swift
+++ AR-BoardGame/AR-BoardGame/View/ContentView.swift
@@ -15,7 +15,6 @@ import RealityKitContent
 
 struct ContentView: View {
     @Environment(ContentViewModel.self) var viewModel: ContentViewModel
-    @State private var enlarge = false
     @State private var showImmersiveSpace = false
 
     @Environment(\.openImmersiveSpace) var openImmersiveSpace
@@ -24,15 +23,17 @@ struct ContentView: View {
     var body: some View {
         RealityView { content in
             // Add the initial RealityKit content
-            if let scene = try? await Entity(named: "Scene", in: realityKitContentBundle) {
-                content.add(scene)
-            }
+//            if let scene = try? await Entity(named: "Scene", in: realityKitContentBundle) {
+//                content.add(scene)
+//            }
+            let bubbleEntity = viewModel.makeBubble("Welcome")
+            bubbleEntity.scale = SIMD3(repeating: 1)
+            let textModelEntity = viewModel.makeTextEntity(text: "Welcome", scale: 0.3)
+            bubbleEntity.addChild(textModelEntity)
+
+            content.add(bubbleEntity)
         } update: { content in
-            // Update the RealityKit content when SwiftUI state changes
-            if let scene = content.entities.first {
-                let uniformScale: Float = enlarge ? 1.4 : 1.0
-                scene.transform.scale = [uniformScale, uniformScale, uniformScale]
-            }
+            
         }
         .onChange(of: showImmersiveSpace) { _, newValue in
             Task {
@@ -52,13 +53,9 @@ struct ContentView: View {
                 }
             }
         }
-        .gesture(TapGesture().targetedToAnyEntity().onEnded { _ in
-            enlarge.toggle()
-        })
         .toolbar {
             ToolbarItemGroup(placement: .bottomOrnament) {
                 VStack (spacing: 12) {
-                    Toggle("Enlarge RealityView Content", isOn: $enlarge)
                     Toggle("Show ImmersiveSpace", isOn: $showImmersiveSpace)
                     Button("Reset ImmersiveNumber") {
                         viewModel.isResetImmersiveContents = true
