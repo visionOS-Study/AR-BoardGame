b22daf96239f44d87ee7e60d4342740cf2ae9119
Dora Choi <135581648+dorachoii@users.noreply.github.com>
2024-09-27 11:24:05 +0900

    feat/순서대로 터뜨리는 로직

--- AR-BoardGame/AR-BoardGame/View/ImmersiveView.swift
+++ AR-BoardGame/AR-BoardGame/View/ImmersiveView.swift
@@ -13,6 +13,8 @@ struct ImmersiveView: View {
     @Environment(ContentViewModel.self) var viewModel: ContentViewModel
     @State private var root: Entity?
     
+    @State private var currentNumber = 0   // 터뜨려야 하는 방울 번호
+    
     var body: some View {
         RealityView (make: { content in
             guard let root else { return }
@@ -25,19 +27,25 @@ struct ImmersiveView: View {
         }
         .gesture(
             SpatialTapGesture()
-            .targetedToAnyEntity()
-            .onChanged { value in
-                
-            }
-            .onEnded { event in
-                // 터치 엔터티 삭제
-                event.entity.removeFromParent()
-                viewModel.addParticleEntity(transForm: event.entity.transform) { entity in
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
-                        entity.removeFromParent()
+                .targetedToAnyEntity()
+                .onChanged { value in
+                    
+                }
+                .onEnded { event in
+                    // currentNum과 bubbleNum 비교!
+                    if let bubbleNumber = Int(event.entity.name.split(separator: "-").last ?? ""),
+                       bubbleNumber == currentNumber {
+                        // 터치 엔터티 삭제
+                        event.entity.removeFromParent()
+                        viewModel.addParticleEntity(transForm: event.entity.transform) { entity in
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                                entity.removeFromParent()
+                            }
+                            currentNumber += 1 
+                        }
                     }
                 }
-            }
+            
         )
         .onChange(of: viewModel.isResetImmersiveContents) { _ ,newValue in
             if newValue {
