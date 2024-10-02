e5dc6121a4c4fa8fa15ee192d72012eae3e9620e
daminoworld <ekadls757@gmail.com>
2024-10-02 19:59:37 +0900

    Add: #8-Create TimerView

---
+++ AR-BoardGame/AR-BoardGame/View/TimerView.swift
@@ -0,0 +1,18 @@
+//
+//  TimerView.swift
+//  AR-BoardGame
+//
+//  Created by Damin on 10/2/24.
+//
+
+import SwiftUI
+
+struct TimerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimerView()
+}
