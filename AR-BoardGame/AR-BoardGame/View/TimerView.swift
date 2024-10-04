e434e816ef650bbbec0fade2fd2d53db0ca5f9c5
daminoworld <ekadls757@gmail.com>
2024-10-04 23:46:30 +0900

    Faet: #8-Add TimerView with reset, back to home button

--- AR-BoardGame/AR-BoardGame/View/TimerView.swift
+++ AR-BoardGame/AR-BoardGame/View/TimerView.swift
@@ -8,11 +8,49 @@
 import SwiftUI
 
 struct TimerView: View {
+    @Environment(ContentViewModel.self) var contentViewModel: ContentViewModel
+    @Environment(TimerViewModel.self) var timerViewModel: TimerViewModel
+    @Binding var showImmersiveSpace: Bool
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Time: \(timerViewModel.formatTime())")
+                .font(.largeTitle)
+                .padding()
+            
+            VStack {
+                
+                Button{
+                    timerViewModel.resetTimer()
+                    contentViewModel.isResetImmersiveContents = true
+                    timerViewModel.startTimer()
+                } label: {
+                    Text("Reset")
+                        .padding()
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                Button {
+                    timerViewModel.stopTimer()
+                    showImmersiveSpace = false
+                } label: {
+                    Text("Back to Home")
+                        .padding()
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+
+            }
+        }
+        .onAppear {
+            timerViewModel.startTimer()
+        }
+        .onChange(of: contentViewModel.isAllBubbleTapped) { _, newValue in
+            if newValue {
+                timerViewModel.stopTimer()
+                contentViewModel.isAllBubbleTapped = false
+            }
+            
+        }
     }
 }
 
-#Preview {
-    TimerView()
-}
